e5180ec546ab6ea983744d8796e58f6efba25cb1
vordomskiy <vordomskiy@gmail.com>
2025-07-28 21:52:23 +0300

    Update test-review.sql
    Рекомендуется при объявлении типов не использовать длину поля max
    Пример:
    varchar(8000)

--- test-review.sql
+++ test-review.sql
@@ -4,7 +4,7 @@ as
 set nocount on
 begin
 	declare @RowCount int = (select count(*) from syn.SA_CustomerSeasonal)
-	,@ErrorMessage varchar(max)
+	,@ErrorMessage varchar(8000)
 
 -- Проверка на корректность загрузки
 	if not exists (
