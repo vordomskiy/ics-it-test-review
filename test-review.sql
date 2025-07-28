3ffac094399a0604d99b5eec718b90f9e9a756e0
vordomskiy <vordomskiy@gmail.com>
2025-07-28 21:53:13 +0300

    Update test-review.sql
    Комментарий пишется непосредственно над строкой кода.
    У комментария тот же уровень отступа, что и у строки/блока, к которому он относится.

--- test-review.sql
+++ test-review.sql
@@ -6,7 +6,7 @@ begin
 	declare @RowCount int = (select count(*) from syn.SA_CustomerSeasonal)
 	,@ErrorMessage varchar(8000)
 
--- Проверка на корректность загрузки
+	-- Проверка на корректность загрузки
 	if not exists (
 	select 1
 	from syn.ImportFile as f
